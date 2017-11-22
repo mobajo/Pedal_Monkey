fcde315ba9c75502a12adaba5cbc73fa7ca2004f
rob <robmitchell@email.com>
2017-11-22 12:26:36 +0100

    Stage and Pitsop models updated and seed created

--- app/models/pitstop.rb
+++ app/models/pitstop.rb
@@ -1,3 +1,6 @@
+
+
+
 class Pitstop < ApplicationRecord
   belongs_to :start_stage, class_name: "Stage", foreign_key: "start_stage_id", optional: :final_stop?
   belongs_to :end_stage, class_name: "Stage", foreign_key: "end_stage_id", optional: :first_stop?
@@ -5,10 +8,25 @@ class Pitstop < ApplicationRecord
   validates :name, presence: true
 
   def final_stop?
-    self.end_stage.trip.stages.count == self.end_stage.stage_no
+    self.end_stage ? self.end_stage.trip.stages.count == self.end_stage.stage_no : false
   end
 
   def first_stop?
-    self.start_stage.stage_no == 1
+    self.start_stage&.stage_no == 1
   end
 end
+
+#class Pitstop < ApplicationRecord
+#  belongs_to :start_stage, class_name: "Stage", foreign_key: "start_stage_id", optional: :final_stop?
+#  belongs_to :end_stage, class_name: "Stage", foreign_key: "end_stage_id", optional: :first_stop?
+
+ # validates :name, presence: true
+
+ # def final_stop?
+ #   self.end_stage.trip.stages.count == self.end_stage.stage_no
+ # end
+
+ # def first_stop?
+ #   self.start_stage.stage_no == 1
+ # end
+#end
