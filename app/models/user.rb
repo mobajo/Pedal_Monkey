7aec7fa75885d3f311ec064f51a4fa208c48083b
rob <robmitchell@email.com>
2017-11-22 17:19:30 +0100

    Cloudinary installed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 has_many :trip_members, dependent: :destroy #Rob said (probably wrong)#
 has_many :trips, through: :trip_members
-
+has_attachment :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
