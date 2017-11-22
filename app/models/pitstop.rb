dab7d4fd66393589d7464bab17c322c595576661
rob <robmitchell@email.com>
2017-11-22 09:06:47 +0100

    Seed completed and alterations to Pitstop Model to allow for start and end pitstop

--- app/models/pitstop.rb
+++ app/models/pitstop.rb
@@ -1,6 +1,14 @@
 class Pitstop < ApplicationRecord
-  belongs_to :start_stage, class_name: "Stage", foreign_key: "start_stage_id"
-  belongs_to :end_stage, class_name: "Stage", foreign_key: "end_stage_id"
+  belongs_to :start_stage, class_name: "Stage", foreign_key: "start_stage_id", optional: :final_stop?
+  belongs_to :end_stage, class_name: "Stage", foreign_key: "end_stage_id", optional: :first_stop?
 
   validates :name, presence: true
+
+  def final_stop?
+    self.end_stage.trip.stages.count == self.end_stage.stage_no
+  end
+
+  def first_stop?
+    self.start_stage.stage_no == 1
+  end
 end
