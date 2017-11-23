294040b8546095852bf087e6a458b104b663b9ae
Morten Jorgensen <morten00@gmail.com>
2017-11-23 15:46:29 +0100

    Possible to create a trip w/stages and corresponding pitstops

--- app/models/pitstop.rb
+++ app/models/pitstop.rb
@@ -2,7 +2,8 @@ class Pitstop < ApplicationRecord
   belongs_to :start_stage, class_name: "Stage", foreign_key: "start_stage_id", optional: :final_stop?
   belongs_to :end_stage, class_name: "Stage", foreign_key: "end_stage_id", optional: :first_stop?
 
-  validates :name, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 
   def final_stop?
     self.end_stage ? self.end_stage.trip.stages.count == self.end_stage.stage_no : false
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,7 @@ class Trip < ApplicationRecord
 	has_many :trip_members
 	has_many :stages, dependent: :destroy
 
-   	validates :title, presence: true
-
+  def find_stage(num)
+    self.stages.find { |stage| stage.stage_no == num }
+  end
 end
