68ab27ef14dfa1d9a8f89f786b6bd21a3c3cfa6f
rob <robmitchell@email.com>
2017-11-29 11:33:05 +0100

    Edit view with list of accomadation for pitstop

--- app/controllers/pitstops_controller.rb
+++ app/controllers/pitstops_controller.rb
@@ -11,16 +11,17 @@ class PitstopsController < ApplicationController
 
 
   def edit
+    @trip = Trip.find(params[:trip_id])
     @pitstop = Pitstop.find(params[:id])
-
+    @hotels = find_places(@pitstop.latitude, @pitstop.longitude)
   end
 
   def update
     trip = Trip.find(params[:trip_id])
     pitstop = Pitstop.find(params[:id])
     pitstop.update(pitstop_params)
-    find_place(pitstop.latitude, pitstop.longitude)
-    redirect_to trip_path(trip )
+    #find_places(@pitstop.latitude, @pitstop.longitude)
+    redirect_to trip_path(trip)
   end
 
   def destroy
@@ -32,17 +33,14 @@ class PitstopsController < ApplicationController
     params.require(:pitstop).permit(:name, :address, :latitude, :longitude, :price, :rating, :start_stage_id, :end_stage_id)
   end
 
-  def find_place(lat, lng)
+  def find_places(lat, lng)
         client = GooglePlaces::Client.new(ENV['GOOGLE_API_BROWSER_KEY'])
     spots = client.spots(lat, lng, :types => 'lodging')
 
-
     spot_array = []
     spots.each do |spot|
-      spot_array << {lat: spot.lat, lng: spot.lng, name: spot.name, vicinity: spot.vicinity, rating: spot.rating}
-     # spot_array << spot.website
+      spot_array << { lat: spot.lat, lng: spot.lng, name: spot.name, vicinity: spot.vicinity, rating: spot.rating }
     end
-     raise
   end
 end
 
