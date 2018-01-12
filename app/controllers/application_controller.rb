a29cb700fafd88d637314ae984dcbbbe50bbd8f5
Morten Jorgensen <morten00@gmail.com>
2018-01-12 18:33:15 +0100

    Now possible to create trip without user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   #protect_from_forgery prepend: true
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:edit, :destroy]
 
   def after_sign_in_path_for(resource)
     dashboard_path
